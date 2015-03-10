6306929ad46bb71ee695835e530ca871b37eb81b
steinbjarne <steinbjarne@hotmail.com>
2015-03-10 13:57:41 +0100

    Using globals for efficiency

--- app/src/main/java/no/curiosity/gameoflife/GameOfLife.java
+++ app/src/main/java/no/curiosity/gameoflife/GameOfLife.java
@@ -2,8 +2,6 @@ package no.curiosity.gameoflife;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -11,13 +9,20 @@ import no.curiosity.gameoflife.vews.GridView;
 
 public class GameOfLife extends ActionBarActivity {
     GridView gridView;
+    Button btnRun;
+    Button btnClear;
+    Button btnStep;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_game_of_life);
+
         gridView = (GridView) findViewById(R.id.viewGrids);
+        btnRun = (Button) findViewById(R.id.btnRun);
+        btnClear = (Button) findViewById(R.id.btnClear);
+        btnStep = (Button) findViewById(R.id.btnStep);
     }
 
     public void clearView(View view) {
@@ -25,25 +30,20 @@ public class GameOfLife extends ActionBarActivity {
     }
 
     public void runGameOfLife(View view) {
-        GridView gridView = (GridView) findViewById(R.id.viewGrids);
-        Button run = (Button) findViewById(R.id.btnRun);
-        Button clear = (Button) findViewById(R.id.btnClear);
-        Button step = (Button) findViewById(R.id.btnStep);
-        if (run.getText().equals("Run")) {
-            run.setText("Stop");
-            clear.setEnabled(false);
-            step.setEnabled(false);
+        if (btnRun.getText().equals("Run")) {
+            btnRun.setText("Stop");
+            btnClear.setEnabled(false);
+            btnStep.setEnabled(false);
         }
         else {
-            run.setText("Run");
-            clear.setEnabled(true);
-            step.setEnabled(true);
+            btnRun.setText("Run");
+            btnClear.setEnabled(true);
+            btnStep.setEnabled(true);
         }
         gridView.runGameOfLife();
     }
 
     public void stepGameOfLife(View view) {
-        GridView gridView = (GridView) findViewById(R.id.viewGrids);
         gridView.stepGameOfLife();
     }
 }
