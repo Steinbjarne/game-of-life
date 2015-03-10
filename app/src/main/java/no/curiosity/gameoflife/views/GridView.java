cba51b2f96ed108d8b0f24c2cba7b512d7e15169
steinbjarne <steinbjarne@hotmail.com>
2015-03-10 11:29:50 +0100

    Changing from ImageView to View

--- app/src/main/java/no/curiosity/gameoflife/views/GridView.java
+++ app/src/main/java/no/curiosity/gameoflife/views/GridView.java
@@ -4,11 +4,12 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.support.annotation.NonNull;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.ImageView;
 
 import no.curiosity.gameoflife.painters.Grid;
 
-public class GridView extends ImageView {
+public class GridView extends View {
     public GridView(Context context) {
         super(context);
     }
